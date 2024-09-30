975a79c0985edb0d3ec6848056f4f9ff8988ed16
Vadim Korolev <vkorol98@gmail.com>
2024-09-30 20:33:44 +0300

    update

---
+++ README.md
@@ -0,0 +1,57 @@
+# Проект по тестированию API Restful-booker.
+
+><a target="_blank" href="https://restful-booker.herokuapp.com/apidoc/index.html#api-Auth">Restful-booker</a>
+> 
+![main page screenshot](pictures/Petstore_API_main_page.png)
+
+---
+### Список проверок, реализованных в автотестах
+1. Получение информации о бронировании по id.
+2. Healthcheck проверка.
+3. Создание бронирования.
+4. Удаление бронирования.
+5. Изменение существующего бронирования.
+
+---
+
+### Используемые инструменты
+<img title="Python" src="pictures/icons/python.svg" height="40" width="40"/> <img title="Pytest" src="pictures/icons/pytest.svg" height="40" width="40"/> <img title="Allure Report" src="pictures/icons/allure_report.png" height="40" width="40"/> <img title="GitHub" src="pictures/icons/github.svg" height="40" width="40"/> <img title="Pycharm" src="pictures/icons/pycharm-original.svg" height="40" width="40"/> <img title="Telegram" src="pictures/icons/telegram.png" height="40" width="40"/> <img title="Jenkins" src="pictures/icons/jenkins-original.svg" height="40" width="40"/> <img title="Requests" src="pictures/icons/requests.png" height="40" width="40"/>  <img title="Jira" src="pictures/icons/jira.svg" height="40" width="40"/>
+
+---
+
+### Запуск автотестов осуществляется с использованием Jenkins
+> [Ссылка на сборку в Jenkins](https://jenkins.autotests.cloud/job/Vadim_Korolev_Petstore_api/)
+
+#### Для запуска автотестов в Jenkins
+1. Открыть [задачу в Jenkins](https://jenkins.autotests.cloud/job/Vadim_Korolev_Petstore_api/)
+
+![jenkins job main page](pictures/Jenkins_job_main_page.png)
+
+2. Нажать "**Build Now**".
+
+---
+
+### Allure отчет
+
+![allure_report page](pictures/allure_report_page.png)
+
+---
+
+### Интеграция с Allure TestOps
+
+> [Job #4397 Vadim Korolev Petstore api](https://allure.autotests.cloud/project/4397)
+
+![allure_testops job](pictures/allure_testops_job.png)
+
+---
+
+### Интеграция с Jira
+> [Задача в Jira](https://jira.autotests.cloud/browse/HOMEWORK-1331)
+ 
+![jira task](pictures/jira_task.png)
+
+---
+
+### Уведомления в Телеграм
+
+![telegram_notification](pictures/tg_notification.png)
---
+++ tests/test_restful_booker.py
@@ -0,0 +1,115 @@
+import allure
+import jsonschema
+from allure_commons.types import Severity
+from restful_booker.utils.load_schema import load_schema
+from restful_booker.utils.help_functions import (get_token, create_booking, put_request,
+                                                      get_request_with_json_response, get_request_without_json_response,
+                                                      post_request, delete_request)
+
+
+@allure.tag("Diploma")
+@allure.tag("API")
+@allure.severity(Severity.NORMAL)
+@allure.feature("Дипломный проект")
+def test_create_booking(default_url):
+    schema = load_schema('create_booking.json')
+    headers = {'Content-Type': 'application/json'}
+    booking = {
+        "firstname": "Jhon",
+        "lastname": "Black",
+        "totalprice": 111000,
+        "depositpaid": False,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+
+    with allure.step('Отправить POST запрос на эндпоинт "/booking"'):
+        result = post_request('/booking', json=booking, headers=headers)
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить json схему ответа'):
+        jsonschema.validate(result.json(), schema)
+    with allure.step(f'Проверить, что значение параметра totalprice в ответе равно 111000'):
+        assert result.json()['booking']['totalprice'] == 111000
+
+
+@allure.tag("Diploma")
+@allure.tag("API")
+@allure.severity(Severity.NORMAL)
+@allure.feature("Дипломный проект")
+def test_get_booking(default_url):
+    id = create_booking()
+    schema = load_schema('get_booking.json')
+    headers = {'Accept': 'application/json'}
+
+    with allure.step(f'Отправить GET запрос на эндпоинт "/booking/{id}"'):
+        result = get_request_with_json_response(f'/booking/{id}', headers=headers)
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить json схему ответа'):
+        jsonschema.validate(result.json(), schema)
+    with allure.step(f'Проверить, что значение параметра bookingid в ответе равно {id}'):
+        assert result.json()['firstname'] == 'Tom'
+
+
+@allure.tag("Diploma")
+@allure.tag("API")
+@allure.severity(Severity.NORMAL)
+@allure.feature("Дипломный проект")
+def test_ping_healthcheck_status_code(default_url):
+    with allure.step('Отправить GET запрос на эндпоинт "/ping"'):
+        result = get_request_without_json_response('/ping')
+    with allure.step('Проверить, что статус код равен 201'):
+        assert result.status_code == 201
+
+
+@allure.tag("Diploma")
+@allure.tag("API")
+@allure.severity(Severity.NORMAL)
+@allure.feature("Дипломный проект")
+def test_delete_booking(default_url):
+    token_func = get_token()
+    token = f'token={token_func}'
+    headers = {'Content-Type': 'application/json',
+               'Cookie': token}
+    id = create_booking()
+    with allure.step(f'Отправить DELETE запрос на эндпоинт "/booking/{id}"'):
+        result = delete_request(f'/booking/{id}', headers=headers)
+    with allure.step('Проверить, что статус код равен 201'):
+        assert result.status_code == 201
+
+
+@allure.tag("Diploma")
+@allure.tag("API")
+@allure.severity(Severity.NORMAL)
+@allure.feature("Дипломный проект")
+def test_update_booking(default_url):
+    token_func = get_token()
+    token = f'token={token_func}'
+    schema = load_schema('update_booking.json')
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Cookie': token}
+    id = create_booking()
+    booking = {
+        "firstname": "Jhon",
+        "lastname": "White",
+        "totalprice": 111000,
+        "depositpaid": False,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    with allure.step(f'Отправить PUT запрос на эндпоинт "/booking/{id}"'):
+        result = put_request(f'/booking/{id}', json=booking, headers=headers)
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить, что значение параметра lastname в ответе равно "White"'):
+        assert result.json()['lastname'] == 'White'
+    with allure.step('Проверить json схему ответа'):
+        jsonschema.validate(result.json(), schema)
